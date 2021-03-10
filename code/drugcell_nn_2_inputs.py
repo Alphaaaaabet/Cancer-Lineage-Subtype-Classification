7fb240a3155f4f86adbc2186a7de3deb54590823
Alphaaaaabet <50163745+Alphaaaaabet@users.noreply.github.com>
2021-03-10 21:49:28 +0800

    Wrapped line at the 80th column

--- code/drugcell_nn_2_inputs.py
+++ code/drugcell_nn_2_inputs.py
@@ -6,7 +6,7 @@ import torch.utils.data as du
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-    
+
 import argparse
 import time
 
@@ -14,48 +14,52 @@ import networkx as nx
 import networkx.algorithms.components.connected as nxacc
 import networkx.algorithms.dag as nxadag
 
+
 class drugcell_nn(nn.Module):
-	###TO DO: update train function to include extra arguments
-	###
-    
+    ###TO DO: update train function to include extra arguments
+    ###
+
     # Need to add terms for number of features, each feature input
-    def __init__(self, term_size_map, term_direct_gene_map, dG, nfeatures, ngene, root, num_hiddens_feature, num_hiddens_genotype, num_cancer_types):
+    def __init__(self, term_size_map, term_direct_gene_map, dG, nfeatures,
+                 ngene, root, num_hiddens_feature, num_hiddens_genotype,
+                 num_cancer_types):
 
         super(drugcell_nn, self).__init__()
 
         self.root = root
-        self.num_hiddens_genotype = num_hiddens_genotype # = 6
-        #self.num_hiddens_drug = num_hiddens_drug
+        self.num_hiddens_genotype = num_hiddens_genotype  # = 6
+        # self.num_hiddens_drug = num_hiddens_drug
         self.num_hiddens_feature = num_hiddens_feature
 
         # dictionary from terms to genes directly annotated with the term
-        self.term_direct_gene_map = term_direct_gene_map   
+        self.term_direct_gene_map = term_direct_gene_map
 
         # calculate the number of values in a state (term): term_size_map is the number of all genes annotated with the term
         self.cal_term_dim(term_size_map)
 
         # ngenes, gene_dim are the number of all genes
         # nfeatures is the number of biological features connected to each gene
-        self.gene_dim = ngene   
-        #self.drug_dim = ndrug
+        self.gene_dim = ngene
+        # self.drug_dim = ndrug
         self.feat_dim = nfeatures
 
         # add modules for neural networks to process genotype features
-        self.construct_direct_biofeature_layer() 
+        self.construct_direct_biofeature_layer()
         self.contruct_direct_gene_layer()
         self.construct_NN_graph(dG)
 
         # add modules for neural networks to process drugs	
-        #self.construct_NN_drug()
+        # self.construct_NN_drug()
 
         # add modules for final layer
         final_input_size = num_hiddens_genotype
-        self.add_module('final_linear_layer', nn.Linear(final_input_size, num_cancer_types))
+        self.add_module('final_linear_layer',
+                        nn.Linear(final_input_size, num_cancer_types))
         self.add_module('final_softmax_layer', nn.Softmax(num_cancer_types))
-        #self.add_module('final_linear_layer', nn.Linear(final_input_size, num_hiddens_final))
-        #self.add_module('final_batchnorm_layer', nn.BatchNorm1d(num_hiddens_final))
-        #self.add_module('final_aux_linear_layer', nn.Linear(num_hiddens_final,1))
-        #self.add_module('final_linear_layer_output', nn.Linear(1, 1))
+        # self.add_module('final_linear_layer', nn.Linear(final_input_size, num_hiddens_final))
+        # self.add_module('final_batchnorm_layer', nn.BatchNorm1d(num_hiddens_final))
+        # self.add_module('final_aux_linear_layer', nn.Linear(num_hiddens_final,1))
+        # self.add_module('final_linear_layer_output', nn.Linear(1, 1))
 
     # calculate the number of values in a state (term)
     # self.derm_dim_map = {GO_term: #_hidden_units}
@@ -70,9 +74,9 @@ class drugcell_nn(nn.Module):
 
             # log the number of hidden variables per each term
             num_output = int(num_output)
-            #print("term\t%s\tterm_size\t%d\tnum_hiddens\t%d" % (term, term_size, num_output))
+            # print("term\t%s\tterm_size\t%d\tnum_hiddens\t%d" % (term, term_size, num_output))
             self.term_dim_map[term] = num_output
-    
+
     # Build a layer for forwarding muations that are directly annotated with a gene
     def construct_direct_biofeature_layer(self):
         """ Iterate through term_direct_gene_map.items() to get gene indices
@@ -80,14 +84,17 @@ class drugcell_nn(nn.Module):
             
             TO DO: Update dimensions to take feature inputs
         """
-        
+
         for term, gene_set in self.term_direct_gene_map.items():
             if len(gene_set) == 0:
-                print(f'There are no directed asscoiated features for {term} {gene_set}')
+                print(
+                    f'There are no directed asscoiated features for {term} {gene_set}')
                 sys.exit(1)
-                
+
             for gene in gene_set:
-                self.add_module(term+str(gene)+'_direct_feature_layer', nn.Linear(self.feat_dim, self.num_hiddens_feature))
+                self.add_module(term + str(gene) + '_direct_feature_layer',
+                                nn.Linear(self.feat_dim,
+                                          self.num_hiddens_feature))
                 # produces array [1, n_hiddens] for each mutation/feature
 
     # build a layer for forwarding gene that are directly annotated with the term
@@ -99,20 +106,25 @@ class drugcell_nn(nn.Module):
             if len(gene_set) == 0:
                 print('There are no directed asscoiated genes for', term)
                 sys.exit(1)
-            
+
             for gene in gene_set:
                 # if there are some genes directly annotated with the term
                 # add a layer taking in all genes and forwarding out only those genes 		
-                self.add_module(term+str(gene)+'_direct_gene_layer', nn.Linear(self.num_hiddens_feature, self.num_hiddens_genotype))
-                self.add_module(term+str(gene)+'_batchnorm_layer', nn.BatchNorm1d(self.num_hiddens_genotype))
-                self.add_module(term+str(gene)+'_aux_linear_layer1', nn.Linear(self.num_hiddens_genotype,1))
-                self.add_module(term+str(gene)+'_aux_linear_layer2', nn.Linear(1,1))
+                self.add_module(term + str(gene) + '_direct_gene_layer',
+                                nn.Linear(self.num_hiddens_feature,
+                                          self.num_hiddens_genotype))
+                self.add_module(term + str(gene) + '_batchnorm_layer',
+                                nn.BatchNorm1d(self.num_hiddens_genotype))
+                self.add_module(term + str(gene) + '_aux_linear_layer1',
+                                nn.Linear(self.num_hiddens_genotype, 1))
+                self.add_module(term + str(gene) + '_aux_linear_layer2',
+                                nn.Linear(1, 1))
 
     # start from bottom (leaves), and start building a neural network using the given ontology
     # adding modules --- the modules are not connected yet
     def construct_NN_graph(self, dG):
 
-        self.term_layer_list = []   # term_layer_list stores the built neural network 
+        self.term_layer_list = []  # term_layer_list stores the built neural network
         self.term_neighbor_map = {}
 
         # term_neighbor_map records all children of each term	
@@ -121,19 +133,19 @@ class drugcell_nn(nn.Module):
         # gives you downstream terms of current GO term
         for term in dG.nodes():
             self.term_neighbor_map[term] = []
-            for child in dG.neighbors(term): 
+            for child in dG.neighbors(term):
                 self.term_neighbor_map[term].append(child)
 
         while True:
             leaves = [n for n in dG.nodes() if dG.out_degree(n) == 0]
-            #leaves = [n for n,d in dG.out_degree().items() if d==0]
-            #leaves = [n for n,d in dG.out_degree() if d==0]
+            # leaves = [n for n,d in dG.out_degree().items() if d==0]
+            # leaves = [n for n,d in dG.out_degree() if d==0]
 
             if len(leaves) == 0:
                 break
 
             self.term_layer_list.append(leaves)
-            
+
             # leaves are only terms connected to genes
             for term in leaves:
 
@@ -143,24 +155,27 @@ class drugcell_nn(nn.Module):
 
                 for child in self.term_neighbor_map[term]:
                     input_size += self.term_dim_map[child]
-                
+
                 # Multiply number_genes * hidden_dim for total feature length 
                 if term in self.term_direct_gene_map:
-                    input_size += len(self.term_direct_gene_map[term])*self.num_hiddens_genotype
+                    input_size += len(self.term_direct_gene_map[
+                                          term]) * self.num_hiddens_genotype
 
                 # term_hidden is the number of the hidden variables in each state
                 term_hidden = self.term_dim_map[term]
 
-                self.add_module(term+'_linear_layer', nn.Linear(input_size, term_hidden))
-                self.add_module(term+'_batchnorm_layer', nn.BatchNorm1d(term_hidden))
-                self.add_module(term+'_aux_linear_layer1', nn.Linear(term_hidden,1))
-                self.add_module(term+'_aux_linear_layer2', nn.Linear(1,1))
-            
+                self.add_module(term + '_linear_layer',
+                                nn.Linear(input_size, term_hidden))
+                self.add_module(term + '_batchnorm_layer',
+                                nn.BatchNorm1d(term_hidden))
+                self.add_module(term + '_aux_linear_layer1',
+                                nn.Linear(term_hidden, 1))
+                self.add_module(term + '_aux_linear_layer2', nn.Linear(1, 1))
+
             # remove leaves, next layer down now has out_degree == 0
             # iterator will repeat until rood node
             # breaks loop when no nodes left
             dG.remove_nodes_from(leaves)
-
 
     # definition of forward function
 
@@ -179,29 +194,27 @@ class drugcell_nn(nn.Module):
         # gene_input = Tensor of gene mutations [batch_size, 3008] each row is cell line
         # drug_input = Tensor of drug represen. [batch_size, 2000] each row is cell line
         # torch.narrow(dimension, start, stop) truncates x along columns (dim=1), from 0 to gene_dim
-        #gene_input = x.narrow(1, 0, self.gene_dim)
-        #drug_input = x.narrow(1, self.gene_dim, self.drug_dim)
-
+        # gene_input = x.narrow(1, 0, self.gene_dim)
+        # drug_input = x.narrow(1, self.gene_dim, self.drug_dim)
 
         # define forward function for GENOTYPE dcell FEATURES-FEATURES-FEATURES-FEATURES-FEATURES-FEATURES-FEATURES-
 
         # Nested dict to store{GO_term:{gene_ind: out_layer}, ...} 
         # term_gene_feature_out_map['GO_term'][gene_ind] accesses the stored activation array
         term_gene_feature_out_map = {}
-        
+
         for term, gene in self.term_direct_gene_map.items():
-            gene_input_1 = x[:,gene]
-            gene_input_2 = y[:,gene]
-            
+            gene_input_1 = x[:, gene]
+            gene_input_2 = y[:, gene]
+
             feat_input = torch.cat([gene_input_1, gene_input_2], dim=1)
-            
+
             term_gene_feature_out_map[term] = {}
-            term_gene_feature_out_map[term][gene] = self._modules[term+gene+'_direct_feature_layer'](feat_input)
-       
-    
-    
+            term_gene_feature_out_map[term][gene] = self._modules[
+                term + gene + '_direct_feature_layer'](feat_input)
+
         # define forward function for GENOTYPE dcell GENE-GENE-GENE-GENE-GENE-GENE-GENE-GENE-GENE-GENE-GENE-GENE-GENE-
-        
+
         # Dict stores {GO_term: [linear_result1, ...]} for each gene connected to GO_term
         term_gene_out_map = {}
         gene_aux_map = {}
@@ -210,17 +223,21 @@ class drugcell_nn(nn.Module):
         # [batch_size, gene_dim] x [gene_dim, num_connected_genes] --> [batch_size, num_connected_genes]
         # generates {GO_term: [linear output tensor]}
         for term, gene_dict in self.term_gene_feature_out_map.items():
-            
+
             for gene, activation in gene_dict.items():
-                
-                gene_layer_output = self._modules[term+gene+'_direct_gene_layer'](activation)
-                tanh_out =  torch.tanh(gene_layer_output)                   
-                term_gene_out_map[term] = self._modules[term+gene+'_batchnorm_layer'](tanh_out)
-                aux_layer1_out = torch.tanh(self._modules[term+gene+'_aux_linear_layer1'](term_gene_out_map[term]))
-                gene_aux_map[term] = self._modules[term+gene+'_aux_linear_layer2'](aux_layer1_out)
-        
+                gene_layer_output = self._modules[
+                    term + gene + '_direct_gene_layer'](activation)
+                tanh_out = torch.tanh(gene_layer_output)
+                term_gene_out_map[term] = self._modules[
+                    term + gene + '_batchnorm_layer'](tanh_out)
+                aux_layer1_out = torch.tanh(
+                    self._modules[term + gene + '_aux_linear_layer1'](
+                        term_gene_out_map[term]))
+                gene_aux_map[term] = self._modules[
+                    term + gene + '_aux_linear_layer2'](aux_layer1_out)
+
         # define forward function for ONTOLOGY dcell ONTOLOGY-ONTOLOGY-ONTOLOGY-ONTOLOGY-ONTOLOGY-ONTOLOGY-ONTOLOGY-
-        
+
         term_NN_out_map = {}
         aux_out_map = {}
 
@@ -238,24 +255,27 @@ class drugcell_nn(nn.Module):
                     child_input_list.append(term_gene_out_map[term])
 
                 # Add layer before this for gene term
-                child_input = torch.cat(child_input_list,1)
+                child_input = torch.cat(child_input_list, 1)
 
-                term_NN_out = self._modules[term+'_linear_layer'](child_input)				
+                term_NN_out = self._modules[term + '_linear_layer'](child_input)
 
                 Tanh_out = torch.tanh(term_NN_out)
-                term_NN_out_map[term] = self._modules[term+'_batchnorm_layer'](Tanh_out)
-                aux_layer1_out = torch.tanh(self._modules[term+'_aux_linear_layer1'](term_NN_out_map[term]))
-                aux_out_map[term] = self._modules[term+'_aux_linear_layer2'](aux_layer1_out)		
+                term_NN_out_map[term] = self._modules[
+                    term + '_batchnorm_layer'](Tanh_out)
+                aux_layer1_out = torch.tanh(
+                    self._modules[term + '_aux_linear_layer1'](
+                        term_NN_out_map[term]))
+                aux_out_map[term] = self._modules[term + '_aux_linear_layer2'](
+                    aux_layer1_out)
 
         # connect two neural networks at the top #################################################
         final_input = term_NN_out_map[self.root]
 
-        out = self._modules['final_softmax_layer'](self._modules['final_linear_layer'](final_input))
+        out = self._modules['final_softmax_layer'](
+            self._modules['final_linear_layer'](final_input))
         term_NN_out_map['final'] = out
 
-        #aux_layer_out = torch.tanh(self._modules['final_aux_linear_layer'](out))
+        # aux_layer_out = torch.tanh(self._modules['final_aux_linear_layer'](out))
         aux_out_map['final'] = out
 
         return aux_out_map, term_NN_out_map
-
-
--- code/util.py
+++ code/util.py
@@ -5,149 +5,151 @@ import networkx.algorithms.components.connected as nxacc
 import networkx.algorithms.dag as nxadag
 import numpy as np
 
-def pearson_corr(x, y):
-	xx = x - torch.mean(x)
-	yy = y - torch.mean(y)
 
-	return torch.sum(xx*yy) / (torch.norm(xx, 2)*torch.norm(yy,2))
+def pearson_corr(x, y):
+    xx = x - torch.mean(x)
+    yy = y - torch.mean(y)
+
+    return torch.sum(xx * yy) / (torch.norm(xx, 2) * torch.norm(yy, 2))
+
 
 def load_ontology(file_name, gene2id_mapping):
+    dG = nx.DiGraph()
+    term_direct_gene_map = {}
+    term_size_map = {}
 
-	dG = nx.DiGraph()
-	term_direct_gene_map = {}
-	term_size_map = {}
+    file_handle = open(file_name)
 
-	file_handle = open(file_name)
+    gene_set = set()
 
-	gene_set = set()
+    for line in file_handle:
 
-	for line in file_handle:
+        line = line.rstrip().split()
 
-		line = line.rstrip().split()
-		
-		if line[2] == 'default':
-			dG.add_edge(line[0], line[1])
-		else:
-			if line[1] not in gene2id_mapping:
-				continue
+        if line[2] == 'default':
+            dG.add_edge(line[0], line[1])
+        else:
+            if line[1] not in gene2id_mapping:
+                continue
 
-			if line[0] not in term_direct_gene_map:
-				term_direct_gene_map[ line[0] ] = set()
+            if line[0] not in term_direct_gene_map:
+                term_direct_gene_map[line[0]] = set()
 
-			# Collect the "child" of genes at position 0
-			term_direct_gene_map[line[0]].add(gene2id_mapping[line[1]])
+            # Collect the "child" of genes at position 0
+            term_direct_gene_map[line[0]].add(gene2id_mapping[line[1]])
 
-			gene_set.add(line[1])
+            gene_set.add(line[1])
 
-	file_handle.close()
+    file_handle.close()
 
-	print('There are', len(gene_set), 'genes')
+    print('There are', len(gene_set), 'genes')
 
-	for term in dG.nodes():
-		
-		term_gene_set = set()
+    for term in dG.nodes():
 
-		if term in term_direct_gene_map:
-			term_gene_set = term_direct_gene_map[term]
+        term_gene_set = set()
 
-		deslist = nxadag.descendants(dG, term)
+        if term in term_direct_gene_map:
+            term_gene_set = term_direct_gene_map[term]
 
-		for child in deslist:
-			if child in term_direct_gene_map:
-				term_gene_set = term_gene_set | term_direct_gene_map[child]
+        deslist = nxadag.descendants(dG, term)
 
-		# jisoo
-		if len(term_gene_set) == 0:
-			print('There is empty terms, please delete term:', term)
-			sys.exit(1)
-		else:
-			term_size_map[term] = len(term_gene_set)
+        for child in deslist:
+            if child in term_direct_gene_map:
+                term_gene_set = term_gene_set | term_direct_gene_map[child]
 
-	leaves = [n for n in dG.nodes if dG.in_degree(n) == 0]
-	#leaves = [n for n,d in dG.in_degree() if d==0]
+        # jisoo
+        if len(term_gene_set) == 0:
+            print('There is empty terms, please delete term:', term)
+            sys.exit(1)
+        else:
+            term_size_map[term] = len(term_gene_set)
 
-	uG = dG.to_undirected()
-	connected_subG_list = list(nxacc.connected_components(uG))
+    leaves = [n for n in dG.nodes if dG.in_degree(n) == 0]
+    # leaves = [n for n,d in dG.in_degree() if d==0]
 
-	print('There are', len(leaves), 'roots:', leaves[0])
-	print('There are', len(dG.nodes()), 'terms')
-	print('There are', len(connected_subG_list), 'connected componenets')
+    uG = dG.to_undirected()
+    connected_subG_list = list(nxacc.connected_components(uG))
 
-	if len(leaves) > 1:
-		print('There are more than 1 root of ontology. Please use only one root.')
-		sys.exit(1)
-	if len(connected_subG_list) > 1:
-		print( 'There are more than connected components. Please connect them.')
-		sys.exit(1)
+    print('There are', len(leaves), 'roots:', leaves[0])
+    print('There are', len(dG.nodes()), 'terms')
+    print('There are', len(connected_subG_list), 'connected componenets')
 
-	return dG, leaves[0], term_size_map, term_direct_gene_map
+    if len(leaves) > 1:
+        print(
+            'There are more than 1 root of ontology. Please use only one root.')
+        sys.exit(1)
+    if len(connected_subG_list) > 1:
+        print('There are more than connected components. Please connect them.')
+        sys.exit(1)
+
+    return dG, leaves[0], term_size_map, term_direct_gene_map
 
 
 def load_train_data(file_name, cell2id):
-	feature = []
-	label = []
+    feature = []
+    label = []
 
-	with open(file_name, 'r') as fi:
-		for line in fi:
-			tokens = line.strip().split('\t')
+    with open(file_name, 'r') as fi:
+        for line in fi:
+            tokens = line.strip().split('\t')
 
-			feature.append(cell2id[tokens[0]])
-			label.append([float(tokens[2])])
+            feature.append(cell2id[tokens[0]])
+            label.append([float(tokens[2])])
 
-	return feature, label
+    return feature, label
 
 
 def prepare_predict_data(test_file, cell2id_mapping_file):
+    # load mapping files
+    cell2id_mapping = load_mapping(cell2id_mapping_file)
+    # drug2id_mapping = load_mapping(drug2id_mapping_file)
 
-	# load mapping files
-	cell2id_mapping = load_mapping(cell2id_mapping_file)
-	#drug2id_mapping = load_mapping(drug2id_mapping_file)
+    test_feature, test_label = load_train_data(test_file, cell2id_mapping)
 
-	test_feature, test_label = load_train_data(test_file, cell2id_mapping)
+    print('Total number of cell lines = %d' % len(cell2id_mapping))
+    # print('Total number of drugs = %d' % len(drug2id_mapping))
 
-	print('Total number of cell lines = %d' % len(cell2id_mapping))
-	#print('Total number of drugs = %d' % len(drug2id_mapping))
-
-	return (torch.Tensor(test_feature), torch.Tensor(test_label)), cell2id_mapping
+    return (torch.Tensor(test_feature),
+            torch.Tensor(test_label)), cell2id_mapping
 
 
 def load_mapping(mapping_file):
+    mapping = {}
 
-	mapping = {}
+    file_handle = open(mapping_file)
 
-	file_handle = open(mapping_file)
+    for line in file_handle:
+        line = line.rstrip().split()
+        mapping[line[1]] = int(line[0])
 
-	for line in file_handle:
-		line = line.rstrip().split()
-		mapping[line[1]] = int(line[0])
+    file_handle.close()
 
-	file_handle.close()
-	
-	return mapping
+    return mapping
 
 
 def prepare_train_data(train_file, test_file, cell2id_mapping_file):
+    # load mapping files
+    cell2id_mapping = load_mapping(cell2id_mapping_file)
+    # drug2id_mapping = load_mapping(drug2id_mapping_file)
 
-	# load mapping files
-	cell2id_mapping = load_mapping(cell2id_mapping_file)
-	#drug2id_mapping = load_mapping(drug2id_mapping_file)
+    train_feature, train_label = load_train_data(train_file, cell2id_mapping)
+    test_feature, test_label = load_train_data(test_file, cell2id_mapping)
 
-	train_feature, train_label = load_train_data(train_file, cell2id_mapping)
-	test_feature, test_label = load_train_data(test_file, cell2id_mapping)
+    print('Total number of cell lines = %d' % len(cell2id_mapping))
+    # print('Total number of drugs = %d' % len(drug2id_mapping))
 
-	print('Total number of cell lines = %d' % len(cell2id_mapping))
-	#print('Total number of drugs = %d' % len(drug2id_mapping))
-
-	return (torch.Tensor(train_feature), torch.FloatTensor(train_label), torch.Tensor(test_feature), torch.FloatTensor(test_label)), cell2id_mapping
+    return (torch.Tensor(train_feature), torch.FloatTensor(train_label),
+            torch.Tensor(test_feature),
+            torch.FloatTensor(test_label)), cell2id_mapping
 
 
 def build_input_vector(input_data, cell_features):
-	genedim = len(cell_features[0,:])
-	#drugdim = len(drug_features[0,:])
-	feature = np.zeros((input_data.size()[0], genedim))
+    genedim = len(cell_features[0, :])
+    # drugdim = len(drug_features[0,:])
+    feature = np.zeros((input_data.size()[0], genedim))
 
-	for i in range(input_data.size()[0]):
-		feature[i] = cell_features[int(input_data[i,0])]
+    for i in range(input_data.size()[0]):
+        feature[i] = cell_features[int(input_data[i, 0])]
 
-	feature = torch.from_numpy(feature).float()
-	return feature
+    feature = torch.from_numpy(feature).float()
+    return feature
